372dddd00b7fe4dba9745ef85139039c57544ffd
Charge <Charge85172@gmail.com>
2025-06-16 14:56:01 +0200

    mapima

--- src/js/game.js
+++ src/js/game.js
@@ -8,6 +8,8 @@ export class Game extends Engine {
             width: 1280,
             height: 720,
             maxFps: 30,
+            pixelRatio: 1,
+            pixelArt: true,
             displayMode: DisplayMode.FitScreen
         })
         this.start(ResourceLoader).then(() => this.setupScenes())
