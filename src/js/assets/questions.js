af076d1334afa954893f760734dcb494cda45df4
cctenshii <SissiWang2003@gmail.com>
2025-06-20 10:02:55 +0200

    questions.js startcode in assets mapje

---
+++ src/js/assets/questions.js
@@ -0,0 +1,27 @@
+export const chinaQuestions = [
+    {
+        text: "What is this creature's symbolism?",
+        options: ["Chaos", "Grace", "Imperial Power", "Courage"],
+        answer: "Imperial Power"
+    },
+    {
+        text: "What is the name of this/his staff?",
+        options: ["Huo Qiang", "Ruyi Jingu Bang", "Huojian Qiang", "Luyi Jingu Bang"],
+        answer: "Ruyi Jingu Bang"
+    },
+    {
+        text: "Which rank do you need to use these fans?",
+        options: ["Royalty", "Maiden", "Officials", "High-ranking Officials"],
+        answer: ["Royalty", "High-ranking Officials"]
+    },
+    {
+        text: "In which dynasty was this used as currency?",
+        options: ["Shang", "Ming", "Song", "Zhou"],
+        answer: "Zhou"
+    },
+    {
+        text: "What is this kind of Chinese porcelain called?",
+        options: ["Qinghua", "Dehua", "Chinghwa", "Baekja"],
+        answer: "Qinghua"
+    }
+];
