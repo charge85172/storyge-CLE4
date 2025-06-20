b5225b2d5b2df49c1ec864f651d120779e3759f0
SukunaRaven <sabrinagenegel@gmail.com>
2025-06-20 13:40:50 +0200

    7 seconden timer voor movement block

--- src/js/items/bench.js
+++ src/js/items/bench.js
@@ -1,56 +1,61 @@
-import { Actor, Vector } from "excalibur";
-import { Resources } from "../resources.js"
-import { Keys } from "excalibur";
+import { Actor, Vector, Keys } from "excalibur";
+import { Resources } from "../resources.js";
 
 export class Bench extends Actor {
-    
     constructor(pos) {
-        super({pos, width: Resources.Bench.width,height: Resources.Bench.height})
-        this.graphics.use(Resources.Bench.toSprite())
-        this.scale = new Vector(0.2, 0.2)
+        super({ pos, width: Resources.Bench.width, height: Resources.Bench.height });
+        this.graphics.use(Resources.Bench.toSprite());
+        this.scale = new Vector(0.2, 0.2);
         this.tag = "furnitureItem";
-       
-               this.cellSize = 100
-       
-               this.allowedPositions = [
-                   { row: 6, column: 4 },
-                   { row: 6, column: 6.5 },
-                   { row: 6, column: 9 },
-               ]
-               this.currentIndex = 0
-               this.placeAtPosition()
-           }
-       
-           placeAtPosition(){
-               if(this.currentIndex >= this.allowedPositions.length) this.currentIndex = 0
-               if(this.currentIndex < 0) this.currentIndex = this.allowedPositions.length -1 
-               let selectedPosition = this.allowedPositions[this.currentIndex]
-       
-               this.pos.x = selectedPosition.column * this.cellSize
-               this.pos.y = selectedPosition.row * this.cellSize
-           }
-       
-           onPreUpdate(engine) {
-       
-               let kb = engine.input.keyboard
-       
-               if (kb.wasPressed(Keys.W) || kb.wasPressed(Keys.Up)) {
-                   this.currentIndex--
-                   this.placeAtPosition()
-               }
-               if (kb.wasPressed(Keys.S) || kb.wasPressed(Keys.Down)) {
-                   this.currentIndex++
-                   this.placeAtPosition() 
-               }
-               if (kb.wasPressed(Keys.A) || kb.wasPressed(Keys.Left)) {
-                   this.currentIndex--
-                   this.placeAtPosition()
-               }
-               if (kb.wasPressed(Keys.D) || kb.wasPressed(Keys.Right)) {
-                   this.currentIndex++
-                   this.placeAtPosition()
-               }
-           }
-           
-    
+
+        this.cellSize = 100;
+
+        this.allowedPositions = [
+            { row: 6, column: 4 },
+            { row: 6, column: 6.5 },
+            { row: 6, column: 9 },
+        ];
+
+        this.currentIndex = 0;
+        this.canMove = true; // Kan bewegen na spawnen
+
+        this.placeAtPosition();
+
+        // 1 seconde om te bewegen
+        setTimeout(() => {
+            this.canMove = false;
+            console.log("Movement disabled after spawn");
+        }, 7000); // Na 7 seconden kan je niet meer bewegen. Je moeder met je item verplaatsen.
+    }
+
+    placeAtPosition() {
+        if (this.currentIndex >= this.allowedPositions.length) this.currentIndex = 0;
+        if (this.currentIndex < 0) this.currentIndex = this.allowedPositions.length - 1;
+        const pos = this.allowedPositions[this.currentIndex];
+        this.pos.x = pos.column * this.cellSize;
+        this.pos.y = pos.row * this.cellSize;
+    }
+
+    onPreUpdate(engine) {
+        if (!this.canMove) return; //Fuck your movement
+
+        const kb = engine.input.keyboard;
+
+        if (kb.wasPressed(Keys.W) || kb.wasPressed(Keys.Up)) {
+            this.currentIndex--;
+            this.placeAtPosition();
+        }
+        if (kb.wasPressed(Keys.S) || kb.wasPressed(Keys.Down)) {
+            this.currentIndex++;
+            this.placeAtPosition();
+        }
+        if (kb.wasPressed(Keys.A) || kb.wasPressed(Keys.Left)) {
+            this.currentIndex--;
+            this.placeAtPosition();
+        }
+        if (kb.wasPressed(Keys.D) || kb.wasPressed(Keys.Right)) {
+            this.currentIndex++;
+            this.placeAtPosition();
+        }
+    }
 }
